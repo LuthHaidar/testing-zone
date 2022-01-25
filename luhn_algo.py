af8417339192202b728bc53d9615b6c8812f1aa6
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-01-25 21:43:18 +0800

    mm

--- luhn_algo.py
+++ luhn_algo.py
@@ -1,25 +1,25 @@
 def luhn(n):
-    temp_list=list(str(n))
-    my_list=[]
-    list1 = temp_list[-2::-2]
-    list2=temp_list[::-2]
-    list2 = [int (n) for n in list2]
-    my_list=[int(n) for n in list1]
-    list1 = [int(n)*2 for n in list1]
-    t_list=list1
-    for el in list1:
+    a=list(str(n))
+    b=[]
+    c = a[-2::-2]
+    d=a[::-2]
+    d = [int (n) for n in d]
+    b=[int(n) for n in c]
+    c = [int(n)*2 for n in c]
+    e=c
+    for el in c:
         sum_res=0
         if el>9:
-            idx = list1.index(el)
-            t_list.pop(idx)
+            idx = c.index(el)
+            e.pop(idx)
             while el:
                 rem = el%10
                 sum_res+=rem
                 el = el//10 
-            t_list.insert(idx, sum_res)
-    list1_sum=sum(t_list)
-    list2_sum = sum(list2)
-    final_sum = list1_sum+ list2_sum
+            e.insert(idx, sum_res)
+    c_sum=sum(e)
+    d_sum = sum(d)
+    final_sum = c_sum+ d_sum
     if final_sum%10==0:
         return True
     return False
