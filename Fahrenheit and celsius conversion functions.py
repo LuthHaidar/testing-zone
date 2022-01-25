415e1462b5dcae87514cb09f72608c3ee9c28cf2
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-01-25 15:10:03 +0800

    Update Fahrenheit and celsius conversion functions.py

--- Fahrenheit and celsius conversion functions.py
+++ Fahrenheit and celsius conversion functions.py
@@ -3,3 +3,11 @@ def celsius_to_fahrenheit(c):
 
 def fahrenheit_to_celsius(f):
     return ((f-32)*5)/9
+
+f_or_c = input("Type F for Fahrenheit to Celsius conversion, and C for Celsius to Fahrenheit conversion: ")
+
+deg = int(input("Type in the temperature to be converted: "))
+
+if f_or_c = "F": fahrenheit_to_celsius(deg)
+    else if f_or_c = "C": celsius_to_fahrenheit(deg)
+    else: pass
