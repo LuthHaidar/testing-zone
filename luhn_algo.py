2dac25e171a77e7a4656418e083133db287e153b
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-02-08 15:09:20 +0800

    Update luhn_algo.py

--- luhn_algo.py
+++ luhn_algo.py
@@ -1,25 +1,25 @@
 def luhn(n):
-    a=list(str(n))
-    b=[]
-    c = a[-2::-2]
-    d=a[::-2]
+    a = list(str(n))
+    b = []
+    c = a[-2 :: -2]
+    d = a[:: -2]
     d = [int (n) for n in d]
-    b=[int(n) for n in c]
-    c = [int(n)*2 for n in c]
-    e=c
+    b = [int(n) for n in c]
+    c = [int(n) * 2 for n in c]
+    e = c
     for el in c:
-        sum_res=0
-        if el>9:
+        sum_res = 0
+        if el > 9:
             id = c.index(el)
             e.pop(id)
             while el:
-                rem = el%10
-                sum_res+=rem
-                el = el//10 
+                rem = el % 10
+                sum_res += rem
+                el = el // 10 
             e.insert(id, sum_res)
-    c_sum=sum(e)
+    c_sum = sum(e)
     d_sum = sum(d)
-    f_sum = c_sum+ d_sum
-    if f_sum%10==0:
+    f_sum = c_sum + d_sum
+    if f_sum % 10 == 0:
         return True
     return False
