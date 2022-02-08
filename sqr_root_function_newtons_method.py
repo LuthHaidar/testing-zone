7e47ac9ccf2d79a4095f176c67ea0171d6ba75d4
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-02-08 23:44:52 +0800

    comments

--- sqr_root_function_newtons_method.py
+++ sqr_root_function_newtons_method.py
@@ -1,8 +1,8 @@
 def sqrt(n):
-    x = n
+    x = n # initial guess
     while (True):
-        rt = 0.5 * (x + (n / x))
-        if (((rt - x) ** 2) ** 0.5) < 0.0000000001:
+        rt = 0.5 * (x + (n / x)) # new guess
+        if (((rt - x) ** 2) ** 0.5) < 0.0000000001: # check if the difference between the new guess and the old guess is less than 0.0000000001
             break
-        x = rt
+        x = rt # update the old guess
     return rt
