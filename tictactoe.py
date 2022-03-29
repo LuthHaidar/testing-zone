7ca5ab18506e38ea6250ee9cd0b6e61a84af57f8
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-03-29 16:45:02 +0800

    commit

--- tictactoe.py
+++ tictactoe.py
@@ -29,7 +29,11 @@ def main():
             print("That space is already taken!")
 
 def intro():
-    print("Welcome to Tic Tac Toe by Luth and Jie Rei")
+    print("Welcome to Tic Tac Toe by Luth and Jie Rei.\n")
+    print("The rules are simple. You play against a friend, or yourself, if you have no friends.\n")
+    print("You can play on the board in the following way:")
+    print("When asked for your row, input your selected row number.\n")
+    print("When asked for your column, input your selected column number.\n")
     wanttoplay = input("Do you want to play? (y/n): ")
     if wanttoplay == "y":
         print("Let's play!")
