394875e16d7d979a308c80fad2c32f7a1af65318
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-03-31 23:03:17 +0800

    fixed it to actually answer the question
    - read_minscore now return the names of all students who passed the minimum score provided
    
    - write_score now includes variable for student's name

--- question-3-alternative-to-parallel-arrays.py
+++ question-3-alternative-to-parallel-arrays.py
@@ -6,7 +6,7 @@ def intro(): #basic intro with instructions
     print('write_score(student, subject, score)\n')
     print('To read the students who passedthe minimum score use the following format:')
     print('read_minscore(subject, minscore)\n')
-    print('Ensure that the subject is a string and the score is an integer or float.')
+    print('Ensure that the student and subject is a string and the score is an integer or float.')
     print('For example:')
     print('write_score("Bob", "Maths", 80)')
     print('write_score("Alice", "Maths", 40)')
@@ -16,7 +16,7 @@ def intro(): #basic intro with instructions
 def write_score(student, subject, score): #function to write a score to the database
     scores.append((student, subject, score))
 
-def read_minscore(subject, minscore):
+def read_minscore(subject, minscore): #function to read students who passed the minimum score
     passes = []
     for i in scores:
         if i[1] == subject and i[2] >= minscore:
