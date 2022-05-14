779425feb7ed89df30fa007bee82f099ccb6dda1
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-05-14 11:09:47 +0800

    fixed palindrome-recursive.py

--- palindrome-recursive.py
+++ palindrome-recursive.py
@@ -1,9 +1,24 @@
-
+'''
 def palindromeTest(string):
     if string[0] == string[-1] and palindromeTest(string[1:-1]):
         return "Its a palindrome"
     else:
         return "Not a palindrome"
-
+'''
 #doesn't actually work
 #no clue why
+
+def palindromeTest(string):
+    string = str(string)
+    string = string.lower()
+    if len(string) == 1:
+        return "Its a palindrome."
+    elif len(string) == 2:
+        if string[0] == string[1]:
+            return "Its a palindrome."
+        else:
+            return "Its not a palindrome."    
+    elif string[0] == string[-1]:
+        return palindromeTest(string[1:-1])
+    else:
+        return "Its not a palindrome."
