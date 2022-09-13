ef98fbb25ba59b4a2ffd1a2ba01871da641c4e88
StrangeFlame <85915348+LuthHaidar@users.noreply.github.com>
2022-09-13 15:49:23 +0800

    mmmmmm commit

--- OOP_Practice_Qn2.py
+++ OOP_Practice_Qn2.py
@@ -10,15 +10,13 @@ class Spaceship:
     
     def go_to(self, planet): # changes the location of the spaceship
         if self.guardian == None or self.guardian == []:
-            print(self.name, " does not have any guardians on board")
+            print(self.name, "does not have any guardians on board")
         else:
             if planet not in self.known_locations:
-                print(planet, " is not a known location")
+                print(planet, "is not a known location")
             else:
                 self.clocation = planet
-                print(self.name, " travels to ", planet)
-                for i in self.guardian:
-                    i.location = self.clocation
+                print(self.name, "travels to", planet)
     
     def contains(self): # returns the name of the spaceship
         return self.guardian
@@ -27,7 +25,9 @@ class Guardian:
     def __init__(self, name, clocation, *known_planets):
         self.name = name
         self.clocation = clocation
-        self.knownplanets = known_planets
+        self.knownplanets = []
+        for i in known_planets:
+            self.knownplanets.append(i)
         self.onboard = False
         if self.clocation not in self.knownplanets:
             self.knownplanets.append(self.clocation)
@@ -37,18 +37,23 @@ class Guardian:
 
     def board(self, ship): # boards the spaceship
         if ship.clocation != self.clocation:
-            print(self.name, " cannot board the ", ship.name)
+            print(self.name, "cannot board the", ship.name)
         else:
             ship.guardian.append(self.name)
-            print(self.name, " boards the ", ship.name)
-            ship.known_locations.append(self.known_lanets)
+            print(self.name, "boards the", ship.name)
+            for i in self.knownplanets:
+                if i not in ship.known_locations:
+                    ship.known_locations.append(i)
+                else:
+                    pass
             self.knownplanets = ship.known_locations
+            self.knownplanets = list(set(self.knownplanets))
             self.clocation = ship.clocation
             self.onboard = True
             self.ship = ship
     
     def location(self):
         if self.onboard == True:
-            print(self.name, " is on the ship ", self.ship.name, " at the planet ", self.ship.clocation)
+            print(self.name, "is on the ship", self.ship.name, "at the planet", self.ship.clocation)
         else:
-            print(self.name, " is on the planet ", self.clocation) 
+            print(self.name, "is on the planet", self.clocation) 
